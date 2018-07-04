4c9316f034041b707df7c03446200c4ec5e94773
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-04 20:17:45 +0100

    create dropdown menu for sorting

--- client/src/components/sort_dropdown.js
+++ client/src/components/sort_dropdown.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SortList = props => {
-  return <div>Sortinglist</div>;
-};
+export default class SortList extends Component {
+  constructor(props) {
+    super(props);
 
-export default SortList;
+    this.state = {
+      optionValue: null
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Sort By:
+          <select
+            onChange={event => console.log(event.target.value)}
+            value={this.state.optionValue ? this.state.optionValue : 'default'}
+          >
+            <option value="default">Default</option>
+            <option value="capacity desc">Capacity Descending</option>
+            <option value="capacity asc">Capacity Ascending</option>
+          </select>
+        </label>
+      </div>
+    );
+  }
+}
