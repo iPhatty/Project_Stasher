304e37d9033256a28db52e3779ac128b9e4e95bb
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-04 15:27:45 +0100

    add styled-components for basic styling

--- client/src/components/search_bar.js
+++ client/src/components/search_bar.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledInput = styled.input`
+  font-size: 1rem;
+  padding: 0.5rem;
+  width: 12rem;
+`;
 
 export default class SearchBar extends Component {
   render() {
-    return(
-      <div>
-        SearchBar
-      </div>
-    )
+    return (
+      <SearchContainer>
+        <StyledInput />
+      </SearchContainer>
+    );
   }
 }
