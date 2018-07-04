e54d87b793b9e15352c5250c45e34fa97e5c214e
Phat Ma <phat.ma@hotmail.co.uk>
2018-07-04 15:57:44 +0100

    create stashlist placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+
 import SearchBar from './components/search_bar';
+import StashList from './components/stash_list';
 
 class App extends Component {
   render() {
     return (
       <div>
         <SearchBar/>
+        <StashList/>
       </div>
     );
   }
